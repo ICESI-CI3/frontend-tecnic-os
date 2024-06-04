c507431e9bf5c5bca81e6875ca738016b407d605
Juan Camilo Gonzalez <93550730+Juank114Gonzalez@users.noreply.github.com>
2024-06-04 08:47:00 -0500

    Update useTechnicians.tsx

--- hooks/technician/useTechnicians.tsx
+++ hooks/technician/useTechnicians.tsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from 'react';
 import { Technician } from '@/interfaces/technician';
 import { TechnicianService } from "@/services/technician.service";
 
 export const useTechnicians = () => {
-   const fetchTechnicians = async () => {
-        const technicianService = new TechnicianService('https://backend-tecnic-os.onrender.com/api');
-        const technicians = await technicianService.getTechnicians();
-        return technicians as Technician[];
-   }
-   
-   return { fetchTechnicians };
+   const [technicians, setTechnicians] = useState<Technician[]>([]);
 
-}
+   useEffect(() => {
+      const fetchTechnicians = async () => {
+         try {
+            const technicianService = new TechnicianService('https://backend-tecnic-os.onrender.com/api');
+            const fetchedTechnicians = await technicianService.getTechnicians();
+            setTechnicians(fetchedTechnicians);
+         } catch (error) {
+            console.error('Error fetching technicians:', error);
+         }
+      };
+
+      fetchTechnicians();
+
+      // Clean-up function
+      return () => {
+         // If needed, perform any clean-up actions here
+      };
+   }, []); // Empty dependency array to run effect only once
+
+   return { technicians };
+};
