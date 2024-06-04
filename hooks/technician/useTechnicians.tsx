1ce875ea3bc4c46e2a3cc31ad5429024139f65a0
Juan Camilo Gonzalez <93550730+Juank114Gonzalez@users.noreply.github.com>
2024-06-04 08:55:25 -0500

    Update useTechnicians.tsx

--- hooks/technician/useTechnicians.tsx
+++ hooks/technician/useTechnicians.tsx
@@ -1,7 +1,7 @@
 import { Technician } from '@/interfaces/technician';
 import { TechnicianService } from "@/services/technician.service";
 
-export const useTechnicians = () => {
+export const UseTechnicians = () => {
    const fetchTechnicians = async () => {
         const technicianService = new TechnicianService('https://backend-tecnic-os.onrender.com/api');
         const technicians = await technicianService.getTechnicians();
