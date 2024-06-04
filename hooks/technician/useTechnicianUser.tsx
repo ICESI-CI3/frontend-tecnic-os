a86e707fce40efeb890b0e3a682b85392a337d21
Juan Camilo Gonzalez <93550730+Juank114Gonzalez@users.noreply.github.com>
2024-06-04 08:34:41 -0500

    Update useTechnicianUser.tsx

--- hooks/technician/useTechnicianUser.tsx
+++ hooks/technician/useTechnicianUser.tsx
@@ -3,7 +3,7 @@ import { TechnicianService } from "@/services/technician.service";
 
 export const useTechnicianUser = (userId: string) => {
     const getUserTechnician = async (userId: string) => {
-          const technicianService = new TechnicianService('http://localhost:3000/api');
+          const technicianService = new TechnicianService('https://backend-tecnic-os.onrender.com/api');
           const userTechnician = await technicianService.getUserTechnician(userId);
           console.log("Client side ---> ", process.env.API_URL)
           return userTechnician as User;
