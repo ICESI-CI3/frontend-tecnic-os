53b49108dbe17e192b3ef4e3f834cb44591c1b64
Juan Camilo Gonzalez <93550730+Juank114Gonzalez@users.noreply.github.com>
2024-06-04 08:35:07 -0500

    Update editUser.tsx

--- hooks/users/editUser.tsx
+++ hooks/users/editUser.tsx
@@ -5,7 +5,7 @@ export const editUser = () => {
     const [error, setError] = useState<string | null>(null); 
 
     const update = async (id: string, userData: {}) => {
-        const userService = new UserService('http://localhost:3000/api');
+        const userService = new UserService('https://backend-tecnic-os.onrender.com/api');
         const user = await userService.update(id, userData);
         return user;
     };
